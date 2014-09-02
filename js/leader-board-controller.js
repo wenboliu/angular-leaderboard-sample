5712e479c619e131dd169c3c420db9bf48e1ed00
Teresast <shenapplymail@163.com>
2014-09-02 18:18:55 +0800

    local merge in LeaderBoardController

--- js/leader-board-controller.js
+++ js/leader-board-controller.js
@@ -1,15 +1,14 @@
 angular.module("leaderBoardApp", [])
-    .controller("LeaderBoardController", function ($scope, $http, $interval) {
+  .controller("LeaderBoardController", function ($scope, $http, $interval) {
+    refresh();
+    $interval(function () {
+        $scope.dataLoaded = true;
         refresh();
-        $interval(function () {
-            $scope.dataLoaded = true;
-            refresh();
-        }, 30000);
-
-        function refresh() {
-            $http.get("/api").success(function (data) {
-                $scope.dataLoaded = false;
-                $scope.items = data;
-            });
-        }
+    }, 30000);
+    function refresh() {
+      $http.get("/api").success(function (data) {
+          $scope.dataLoaded = false;
+          $scope.items = data;
     });
+  }
+});
