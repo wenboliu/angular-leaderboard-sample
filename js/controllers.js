4b1b3d35826910d70e5a9eb5a1f1d7c9231bc96e
Teresast <shenapplymail@163.com>
2014-09-02 15:13:55 +0800

    add space after 'days', add conditional in controller

--- js/controllers.js
+++ js/controllers.js
@@ -1,5 +1,5 @@
 'use strict';
-angular.module('appTime',[]);
+var app = angular.module('appTime',[]);
 
 function countController($scope, $interval){
 	$scope.showDay = false;
@@ -7,17 +7,19 @@ function countController($scope, $interval){
     $scope.time_stop = new Date(2014, 9, 8, 15, 30);
 	$scope.countDown = ($scope.time_stop.getDate() - $scope.currentDate.getDate())*86400 + ($scope.time_stop.getHours() - $scope.currentDate.getHours())*3600 + ($scope.time_stop.getMinutes() - $scope.currentDate.getMinutes())*60 + ($scope.time_stop.getSeconds() - $scope.currentDate.getSeconds());
     var remiander;
-    $interval(function(){
-    	$scope.countDown--
-    	$scope.days = Math.floor($scope.countDown / 86400);
-    	if($scope.days >= 2){
-    		$scope.showDay = true;
-    	}
-    	remiander = $scope.countDown % 86400;
-    	$scope.hours = Math.floor(remiander / 3600)
-    	remiander = remiander % 3600;
-    	$scope.mins = Math.floor(remiander / 60);
-    	$scope.seconds = remiander % 60;
-    },1000,0);  
+    if ($scope.countDown > 0){
+        $interval(function(){
+        	$scope.countDown--
+        	$scope.days = Math.floor($scope.countDown / 86400);
+        	if($scope.days >= 2){
+        		$scope.showDay = true;
+        	}
+        	remiander = $scope.countDown % 86400;
+        	$scope.hours = Math.floor(remiander / 3600)
+        	remiander = remiander % 3600;
+        	$scope.mins = Math.floor(remiander / 60);
+        	$scope.seconds = remiander % 60;
+        },1000,0);  
+    }
     
 }
