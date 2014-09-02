8580a3518979ba5c09d800ccc906e991fe35fdb4
Zhang JiuKun <jkzhang@thoughtworks.com>
2014-09-02 17:49:14 +0800

    config for heroku

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 var connect = require('connect');
 var http = require('http');
 var app = connect();
-app.use('/public', connect.static("../angular-leaderboard-sample"));
+app.use('/', connect.static("."));
 app.use(function (request, response, next) { //root dir
     if ('/api' != request.url) return next();
     http.get({
-        host: '10.18.0.254',
-        port: 3128,
         path: 'http://material-code.appspot.com/test/api'
     }, function (res) {
         res.on("data", function (chunk) {
