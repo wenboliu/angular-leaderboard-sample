dbb5901a091a3b65a7f0beb311294b249f5b9607
Wei Jia <wjia@CNwjia.local>
2014-09-04 09:43:15 +0800

    refactor code

--- js/leader-board-controller.js
+++ js/leader-board-controller.js
@@ -1,23 +1,22 @@
-angular.module("leaderBoardApp", [])
-    .controller("LeaderBoardController", function ($scope, $http, $interval) {
-        angular.element('#items').show();
-        $scope.showListClassName = "hidden";
+app.controller("LeaderBoardController", function ($scope, $http, $interval) {
+    angular.element('#items').show();
+    $scope.showListClassName = "hidden";
+    refresh();
+    $interval(function () {
+        $scope.dataLoaded = true;
         refresh();
-        $interval(function () {
-            $scope.dataLoaded = true;
-            refresh();
-        }, 30000);
-        function refresh() {
-            $scope.showListClassName = "table-responsive";
-            $scope.errorMessage = "";
-            $http.get("/api").then(function (response) {
-                    $scope.dataLoaded = false;
-                    $scope.items = response.data;
-                },
-                function () {
-                    $scope.errorMessage = "Error occurs when getting data from server!"
-                }).then(function () {
-                    $scope.showListClassName = "table-responsive show-list-change";
-                });
-        }
-    });
+    }, 30000);
+    function refresh() {
+        $scope.showListClassName = "table-responsive";
+        $scope.errorMessage = "";
+        $http.get("/api").then(function (response) {
+                $scope.dataLoaded = false;
+                $scope.items = response.data;
+            },
+            function () {
+                $scope.errorMessage = "Error occurs when getting data from server!"
+            }).then(function () {
+                $scope.showListClassName = "table-responsive show-list-change";
+            });
+    }
+});
