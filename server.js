85a82d869a530fbb9a412eee1dd29fdf97cc2460
Teresast <shenapplymail@163.com>
2014-09-05 16:55:10 +0800

    Tong:Add cache.json, when get api failed will load cache file

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
 var connect = require('connect');
 var http = require('http');
+var fs = require('fs');
+
 var app = connect();
 app.use('/', connect.static("."));
 app.use(function (request, response, next) {
     if ('/api' != request.url) return next();
     http.get('http://material-code.appspot.com/test/api', function (res) {
         res.on("data", function (chunk) {
-            response.end(chunk)
+            if(chunk.toString().indexOf("Cannot GET") != 0) {
+                response.end(chunk);
+                fs.writeFile('./cache/cache.json', chunk, function (err) {
+                    if (err) {
+                        console.log("write [./cache/cache.json] failed!" + err);
+                    } else {
+                        console.log("yeah!");
+                    }
+                });
+            }
+            else{
+                fs.readFile('./cache/cache.json', function (err, data) {
+                    if (err){
+                        console.log("read [./cache/cache.json] failed!" + err);
+                    }else {
+                        response.end(data);
+                    }
+                })
+            }
         });
     }).on('error', function (e) {
         console.log("Got error: " + e.message);
