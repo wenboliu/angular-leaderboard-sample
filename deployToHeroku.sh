46f54e8438261f9f0a2bb6607f40613a2153284e
Wei Jia <wjia@CNwjia.local>
2014-09-05 14:53:23 +0800

    updating  heroku deployment script

--- deployToHeroku.sh
+++ deployToHeroku.sh
@@ -2,6 +2,6 @@
 #set -x
 
 git remote add heroku git@heroku.com:leaderboard-angular-sample.git
-git rm node_modules
+git rm -rf node_modules
 git commit -am 'removing node_modules'
 git push -f heroku karma-jasmine:master
