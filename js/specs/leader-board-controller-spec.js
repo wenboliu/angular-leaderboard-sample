0006d984b11b50ce7171de7e3793c6c405e6f165
Teresast <shenapplymail@163.com>
2014-09-05 09:59:19 +0800

    Tong: Add test when get data error

--- js/specs/leader-board-controller-spec.js
+++ js/specs/leader-board-controller-spec.js
@@ -35,5 +35,34 @@ define(['angular','angular-mocks','leaderboard'],function(leaderboard) {
 			expect(mockScope.items).toEqual(items);
 		});
 
+        it("should get errorMessage null when getting successful data", function() {
+            expect(mockScope.errorMessage).toEqual(null);
+        });
+
 	});
+
+    describe("leaderboard failure", function() {
+        var mockScope = {};
+        var controller;
+        var backend;
+        var mockInterval;
+
+        beforeEach(angular.mock.module("leaderBoardApp"));
+
+        beforeEach(angular.mock.inject(function($httpBackend){
+            backend = $httpBackend;
+            backend.expect("GET", "/api").respond(500, '');
+        }));
+
+        beforeEach(angular.mock.inject(function($controller, $rootScope, $http, $interval){
+            mockScope = $rootScope.$new;
+            mockInterval = $interval;
+            controller = $controller("LeaderBoardController", {$scope: mockScope, $http:$http, $interval: mockInterval});
+            backend.flush();
+        }));
+
+        it("should get a valuable errorMessage when getting data failure", function() {
+            expect(mockScope.errorMessage).toEqual("Error occurs when getting data from server!");
+        });
+    })
 });
