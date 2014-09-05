062551ab56c28007c77a9fcf03581d7f76c6663b
Liu Wenbo <wbliu@thoughtworks.com>
2014-09-05 10:05:40 +0800

    wenbo - change the time logic

--- js/src/leader-board-timer.js
+++ js/src/leader-board-timer.js
@@ -1,45 +1,45 @@
 define('leaderboardtimer', ['angular', 'angular-resource'], function(angular) {
 	var leaderBoardApp = angular.module("leaderBoardTimerApp", []);
 
-		leaderBoardApp.directive('timer', function($interval){
+	leaderBoardApp.directive('timer', function($interval){
 		return {link: function(scope, element, attrs){
 			var cssSelector = attrs['timer'];
 			angular.element("." + cssSelector).show();
 			scope.showDay = false;
 
-			var currentDate = new Date();
-			var timeStop = new Date(2014, 8, 8, 15, 30);
-			var countDown = Math.floor((timeStop - currentDate)/1000);
+			$interval(function(){
+				var currentDate = new Date();
+				var timeStop = new Date(2014, 8, 8, 15, 30);
+				var time = calculateTimeDistance(timeStop, currentDate);
+				scope.days = time.days;
+				scope.showDay = scope.days > 0;
+				scope.hours = ("0" + time.hours).slice(-2);
+				scope.mins = ("0" + time.minutes).slice(-2);
+				scope.seconds = ("0" + time.seconds).slice(-2)
 
-			var remiander;
-			if (countDown > 0){
-				$interval(function(){
-					countDown--;
-					scope.days = Math.floor(countDown / 86400);
-					remiander = countDown % 86400;
+			},1000,0);  
 
-					if(scope.days >= 2){
-						scope.showDay = true;
-						scope.hours = ("0" +  Math.floor(remiander / 3600)).slice(-2);
-					}
-					else{
-						scope.hours = ("0" + Math.floor(scope.countDown / 3600)).slice(-2);
-					}
-
-				scope.hours = ("0" + scope.hours).slice(-2);
-				remiander = remiander % 3600;
-				scope.mins = ("0" + Math.floor(remiander / 60)).slice(-2);
-				scope.seconds = ("0" + remiander % 60).slice(-2);
-
-				},1000,0);  
+			function calculateTimeDistance(end, start) {
+				var timeDistanceInSeconds = Math.floor((end - start) / 1000);
+				var seconds = timeDistanceInSeconds % 60;
+				var timeDistanceInMinute = Math.floor(timeDistanceInSeconds / 60);
+				var minutes = timeDistanceInMinute % 60;
+				var timeDistanceInHours = Math.floor(timeDistanceInMinute / 60);
+				var hours = timeDistanceInHours;
+				var days = 0;
+				if(timeDistanceInHours > 48) {
+					hours = timeDistanceInHours % 24;
+					days = Math.floor(timeDistanceInHours / 24);
+				}
+				return {'days': days, 'hours': hours, 'minutes': minutes, 'seconds': seconds};
 			}
 
 		},
 		       restrict:'A',
-		templateUrl: "/js/src/timer.html",
-		scope: true}}).controller('timerCtrl', function(){
-		});
-        angular.element('#secRow').ready(function() {
+		       templateUrl: "/js/src/timer.html",
+		       scope: true}}).controller('timerCtrl', function(){
+		       });
+	angular.element('#secRow').ready(function() {
 		angular.bootstrap('#secRow', ['leaderBoardTimerApp']);
 	});
 
