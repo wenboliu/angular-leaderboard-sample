812bea56031b7e767735d0e5156837cab95e6c51
Liu Wenbo <wbliu@thoughtworks.com>
2014-09-09 10:12:07 +0800

    wenbo - rough framework

---
+++ app/scripts/controllers/leaderBoardController.js
@@ -0,0 +1,27 @@
+var angular = require('angular');
+var angularResource = require('angular-resource');
+
+var leaderBoardController = function ($scope, $http, $interval) {
+	refresh();
+	$interval(function () {
+		refresh();
+	}, 10000);
+	function refresh() {
+		angular.element('#items').show();
+		$scope.showListClassName = "hidden";
+		$scope.dataLoaded = true;
+		$scope.showListClassName = "table-responsive";
+		$scope.errorMessage = null;
+		$http.get("/api").then(function (response) {
+			$scope.items = response.data;
+		},
+		function () {
+			$scope.errorMessage = "Error occurs when getting data from server!"
+		}).then(function () {
+			$scope.dataLoaded = false;
+			$scope.showListClassName = "table-responsive show-list-change";
+		});
+	}
+};
+
+module.exports = leaderBoardController;
---
+++ app/scripts/main.js
@@ -0,0 +1,9 @@
+var leaderBoardController = require('./controllers/leaderBoardController');
+
+
+var leaderBoardApp = angular.module("leaderBoardApp", []);
+leaderBoardApp.controller("LeaderBoardController",leaderBoardController); 
+
+angular.element('#tablelist').ready(function() {
+	angular.bootstrap('#tablelist', ['leaderBoardApp']);
+});
